edf4650d10e0ac23acdc82789a049e157f9718d9
Skyline1503 <flaviangodemont@hotmail.com>
2023-04-10 01:15:50 +0200

    Check if this is the end of the game

--- lib/model/Board.dart
+++ lib/model/Board.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'package:tictactoe/model/Player.dart';
+
 class Board {
   late int nbCase;
   late List<String> board;
@@ -7,6 +9,14 @@ class Board {
   Board({required this.nbCase}) {
     //Génére la liste avec nbCase*nbCase (vide)
     board = List.filled(nbCase * nbCase, "");
+  }
+
+  int getNbCase() {
+    return nbCase;
+  }
+
+  void setNbCase(int nb) {
+    nbCase = nb;
   }
 
   int getSize() {
@@ -22,13 +32,82 @@ class Board {
   }
 
   //Génére position aléatoire
-  void generateRandomPosition() {
+  void generateRandomPosition(String pawn) {
     if (board.contains("")) {
       int randomIndex = Random().nextInt(board.length);
       while (board[randomIndex] != "") {
         randomIndex = Random().nextInt(board.length);
       }
-      board[randomIndex] = "O";
+      board[randomIndex] = pawn;
     }
+  }
+
+  bool isWinner(String player) {
+    //Vérifie les lignes
+    for (int i = 0; i < getNbCase() * getNbCase(); i += getNbCase()) {
+      bool line = true;
+      for (int j = 0; j < getNbCase(); j++) {
+        if (getPosition(i + j) != player) {
+          line = false;
+          break;
+        }
+      }
+      if (line) {
+        return true;
+      }
+    }
+
+    //Vérifie les colonnes
+    for (int i = 0; i < getNbCase(); i++) {
+      bool col = true;
+      for (int j = 0; j < getNbCase(); j++) {
+        if (getPosition(i + j * getNbCase()) != player) {
+          col = false;
+          break;
+        }
+      }
+      if (col) {
+        return true;
+      }
+    }
+
+    //Vérifie la diagonale 1
+    bool diag1 = true;
+    for (int i = 0; i < getNbCase(); i++) {
+      if (getPosition(i * (getNbCase() + 1)) != player) {
+        diag1 = false;
+        break;
+      }
+    }
+    if (diag1) {
+      return true;
+    }
+
+    //Vérifie la diagonale 2
+    bool diag2 = true;
+    for (int i = 0; i < getNbCase(); i++) {
+      if (getPosition((i + 1) * (getNbCase() - 1)) != player) {
+        diag2 = false;
+        break;
+      }
+    }
+    if (diag2) {
+      return true;
+    }
+
+    //Renvoie false pour match nul
+    return false;
+  }
+
+  bool isEndGame(Player p1, Player p2) {
+    if (!board.contains("") || isWinner(p1.getPawn()) || isWinner(p2.getPawn())) {
+      return true;
+    }
+    return false;
+  }
+
+  void resetBoard()
+  {
+    board = List.filled(nbCase * nbCase, "");
   }
 }
--- lib/screens/BoardScreen.dart
+++ lib/screens/BoardScreen.dart
@@ -14,20 +14,33 @@ class BoardScreen extends StatefulWidget {
 class _BoardScreenState extends State<BoardScreen> {
   late Board board;
   late Player player;
+  late Player computer;
   late int nbCase = 4;
+  bool isEndGame = false;
 
   @override
   void initState() {
     super.initState();
     board = Board(nbCase: nbCase);
     player = Player("X");
+    computer = Player("O");
   }
 
   void play(int index) {
-    if (board.getPosition(index) == "") {
+    if (!isEndGame) {
       setState(() {
         board.setPosition(index, player.getPawn());
-        board.generateRandomPosition();
+        isEndGame = board.isEndGame(player, computer);
+      });
+      if (!isEndGame) {
+        setState(() {
+          board.generateRandomPosition(computer.pawn);
+          isEndGame = board.isEndGame(player, computer);
+        });
+      }
+    } else {
+      setState(() {
+        isEndGame = true;
       });
     }
   }
@@ -35,32 +48,54 @@ class _BoardScreenState extends State<BoardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        //App Bar avec titre et retour menu
-        appBar: AppBar(title: const Text("Tic Tac Toe")),
-        body: GridView.count(
-            //Max de case par ligne
-            crossAxisCount: nbCase,
-            //Génére le nombre de Case dans la GridView, index = position de la case
-            children: List.generate(nbCase * nbCase, (index) {
-              //Détecte si le container à été touché
-              return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      play(index);
-                    });
-                  },
-                  //Container qui contient la valeur de la case.
-                  child: Container(
-                    color: Colors.black12,
-                    margin: const EdgeInsets.all(4),
-                    child: Center(
-                      child: Text(
-                        board.getPosition(index),
-                        style:
-                            const TextStyle(fontSize: 32, color: Colors.white),
-                      ),
+      //App Bar avec titre et retour menu
+      appBar: AppBar(title: const Text("Tic Tac Toe")),
+      body: Column(
+        children: [
+          Expanded(
+              flex: 3,
+              child: GridView.count(
+                  //Max de case par ligne
+                  crossAxisCount: nbCase,
+                  //Génére le nombre de Case dans la GridView, index = position de la case
+                  children: List.generate(nbCase * nbCase, (index) {
+                    //Détecte si le container à été touché
+                    return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            play(index);
+                          });
+                        },
+                        //Container qui contient la valeur de la case.
+                        child: Container(
+                          color: Colors.black12,
+                          margin: const EdgeInsets.all(4),
+                          child: Center(
+                            child: Text(
+                              board.getPosition(index),
+                              style: const TextStyle(
+                                  fontSize: 32, color: Colors.black),
+                            ),
+                          ),
+                        ));
+                  }))),
+          Expanded(
+            child: isEndGame
+                ? Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          board.resetBoard();
+                          isEndGame = false;
+                        });
+                      },
+                      child: const Text('Rejouer'),
                     ),
-                  ));
-            })));
+                  )
+                : Center(),
+          ),
+        ],
+      ),
+    );
   }
 }
